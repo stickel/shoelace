e429bafdffad178b8cd75e28146a72c324ba1dba
stickel <mike@screenflicker.com>
2012-11-08 22:24:11 -0800

    add install generator file copying

--- lib/generators/shoelace/install/install_generator.rb
+++ lib/generators/shoelace/install/install_generator.rb
@@ -4,14 +4,29 @@ module Shoelace
   module Generators
     class InstallGenerator < ::Rails::Generators::Base
 
-      source_root File.expand_path('../templates', __FILE__)
-
       desc "This generator installs Shoelace into the Asset Pipeline"
+
+      source_root File.expand_path('../templates', __FILE__)
 
       # Add options here... JS library, template engine (haml, erb), layouts (html5 boilerplate), reset (normalize or reset)
 
       def add_assets
-        
+        if File.exists?('app/assets/stylesheets/application.css')
+          # Make sure the CSS will be included in the pipeline
+          stylesheet = File.read('app/assets/stylesheets/application.css')
+          unless stylesheet.match(/require_tree\s+\.\s*$/)
+            insert_into_file 'app/assets/stylesheets/application.css', " *= require shoelace\n", :after => "require self\n"
+          end
+        else
+          # Copy the application.css file
+          copy_file 'application.css', 'app/assets/stylesheets/application.css'
+        end
+        # Copy shoelace asset files
+        Dir.foreach(File.expand_path('../templates', __FILE__)) do |f|
+          exceptions = ['.', '..', 'application.css']
+          next if exceptions.include?(f)
+          copy_file f, "app/assets/stylesheets/#{f}"
+        end
       end
     end
   end
